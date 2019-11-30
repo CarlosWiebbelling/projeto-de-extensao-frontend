afc682794e7d913caea504f04b7955576a193375
Gabriel Pucinelli <gabrielpucinelli@gmail.com>
2019-11-29 21:19:19 -0300

    Add forms

--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -3,29 +3,42 @@ import { useSelector, useDispatch } from 'react-redux'
 
 import Card from "../Organisms/Card/index";
 import FormProject from '../Organisms/Form/ProjectForm'
+import UpdateUserForm from '../Organisms/Form/UpdateUserForm'
+import EventForm from '../Organisms/Form/EventForm'
 
-import { getAllProjects, postProject } from '../Redux/Actions/ProjectActions'
+import { postProject } from '../Redux/Actions/ProjectActions'
 
 const Project = () => {
     const dispatch = useDispatch()
     const projects = useSelector(state => state.Project)
 
-    useEffect(() => {
-        dispatch(getAllProjects())
-    }, [dispatch])
+    // useEffect(() => {
+    //     dispatch(getProjects())
+    // }, [dispatch])
 
 
     const handleForm = values => {
         dispatch(postProject(values))
     }
-    
+
     return (
-        <div style={{ display: 'flex' }}>
-            <div className="col-3" style={{ backgroundColor: '#e6e6e6', height: '320px' }}>
-                <FormProject handleSubmit={ handleForm } />
-            </div>
-            <div className="col-9">
-                <Card projects={ projects } />
+        <div className="container">
+            <div className="row">
+                <div className="col-3" style={{ backgroundColor: '#e6e6e6', margin: '8px' }}>
+                    <FormProject handleSubmit={handleForm} />
+                </div>
+                <div className="col-3" style={{ backgroundColor: '#e6e6e6', margin: '8px' }}>
+                    <UpdateUserForm handleSubmit={() => console.log('NICE updateUser')} />
+                </div>
+                <div className="col-3" style={{ backgroundColor: '#e6e6e6', margin: '8px' }}>
+                    <EventForm handleSubmit={() => console.log('NICE event')} />
+                </div>
+                <div className="col-9">
+                    <Card projects={projects} />
+                </div>
+                {/* <div className="col-9">
+                    <Card events={events} />
+                </div> */}
             </div>
         </div>
     )
