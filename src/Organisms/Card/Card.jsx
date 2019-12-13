8e647d464a9acb12a6a2c377ffb9eea383157273
DiRibeiro <diribeiro740@gmail.com>
2019-12-13 20:37:48 -0300

    Add user admin verification to delete projects

--- src/Organisms/Card/Card.jsx
+++ src/Organisms/Card/Card.jsx
@@ -14,12 +14,20 @@ const Card = ({ projects, deleteProject, openModal }) => {
 		})
 	}
 
+	const fakeAdmin = {
+		email: "pablo3@gmail.com",
+		name: "Pablo 3",
+		_id: "5df415a70970182295f3d177",
+		level: 3
+	}
 	const user = useSelector(state => state.User.currentLogged)
 
 	user.level = 3;
 
 	console.log(user)
 	console.log(projects)
+
+	const isAdminOfProject = project => project.projectAdmins.filter(admin => fakeAdmin._id === admin._id).length > 0
 
 	const renderProjects = () =>
 		projects.projects.map(project => (
@@ -29,7 +37,7 @@ const Card = ({ projects, deleteProject, openModal }) => {
 						<h4 className='titleProject'>
 							{project.name}
 
-							{user.level === 3 && (
+							{user.level === 3 && isAdminOfProject(project) && (
 								<i
 									className='fas fa-close'
 									onClick={() =>
